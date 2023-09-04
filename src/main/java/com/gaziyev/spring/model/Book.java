bf2155de8b38cb07d7cc5712a543f97137541989
alimfor <muxa1980.mg@gmail.com>
2023-09-04 14:36:31 +0600

    added style for expired book

--- src/main/java/com/gaziyev/spring/model/Book.java
+++ src/main/java/com/gaziyev/spring/model/Book.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AccessLevel;
-import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
